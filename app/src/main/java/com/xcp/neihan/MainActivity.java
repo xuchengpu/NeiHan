2a91b534c7fd4a276b8ca6e098c4a341ceef8968
xuchengpu <1550540124@qq.com>
2018-05-24 19:18:08 +0800

    打造自己的IOC注解

--- app/src/main/java/com/xcp/neihan/MainActivity.java
+++ app/src/main/java/com/xcp/neihan/MainActivity.java
@@ -21,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         IOCUtils.inject(this);
-
         tvHello.setText("ioc注解");
     }
 
